f88c79e5b7cad6d4805c19642dd0a12841ca5fad
John Lalor <john.lalor@nd.edu>
2023-05-02 14:53:14 -0400

    test

--- src/mc/run_multiple_choice.py
+++ src/mc/run_multiple_choice.py
@@ -53,6 +53,19 @@ from transformers.utils import check_min_version
 
 logger = logging.getLogger(__name__)
 
+class CustomTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        attn_masks = inputs.get("attention_mask")
+        difficulties = np.sum(attn_masks, axis=1)
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+        # compute custom loss (suppose one has 3 labels with different weights)
+        loss_fct = nn.CrossEntropyLoss(weight=torch.tensor(difficulties))
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
+
 
 @dataclass
 class ModelArguments:
