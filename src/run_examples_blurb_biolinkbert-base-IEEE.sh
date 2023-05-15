f1635a056de70a66ddfeabbbd295790c4a9ae0b5
John Lalor <john.lalor@nd.edu>
2023-05-15 12:10:17 -0400

    new scrtipt

---
+++ src/run_examples_blurb_biolinkbert-base-IEEE.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+#$ -q gpu@@lalor           # Specify queue
+#$ -l gpu_card=2
+#$ -pe smp 1
+#$ -N LinkDblurbIEEE       # Specify job name
+#$ -t 1-6
+
+module load cuda      # Required modules
+module load cudnn
+source activate linkbert
+
+echo $SGE_TASK_ID
+
+
+export MODEL=BioLinkBERT-base
+export MODEL_PATH=michiyasunaga/$MODEL
+
+
+############################### QA: PubMedQA ###############################
+if [[ $SGE_TASK_ID -eq 1 ]]; then
+baseline=baseline
+task=pubmedqa_hf
+datadir=~/data/linkbert/seqcls/$task
+outdir=runs/$baseline/$task/$MODEL
+mkdir -p $outdir
+python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH \
+  --train_file $datadir/train.json --validation_file $datadir/dev.json --test_file $datadir/test.json \
+  --do_train --do_eval --do_predict \
+  --per_device_train_batch_size 16 --gradient_accumulation_steps 1 --fp16 \
+  --learning_rate 2e-5 --warmup_steps 100 --num_train_epochs 30 --max_seq_length 512 \
+  --save_strategy no --evaluation_strategy no --output_dir $outdir --overwrite_output_dir \
+  --baseline $baseline \
+  |& tee $outdir/log.txt 
+
+############################### QA: BioASQ ###############################
+elif [[ $SGE_TASK_ID -eq 2 ]]; then
+baseline=baseline
+task=bioasq_hf
+datadir=~/data/linkbert/seqcls/$task
+outdir=runs/$baseline/$task/$MODEL
+mkdir -p $outdir
+python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH \
+  --train_file $datadir/train.json --validation_file $datadir/dev.json --test_file $datadir/test.json \
+  --do_train --do_eval --do_predict \
+  --per_device_train_batch_size 16 --gradient_accumulation_steps 1 --fp16 \
+  --learning_rate 2e-5 --warmup_steps 100 --num_train_epochs 20 --max_seq_length 512 \
+  --save_strategy no --evaluation_strategy no --output_dir $outdir --overwrite_output_dir \
+  --baseline $baseline \
+  |& tee $outdir/log.txt 
+
+############## DIFFLENGTH #########################
+############################### QA: PubMedQA ###############################
+elif [[ $SGE_TASK_ID -eq 3 ]]; then
+baseline=difflength
+task=pubmedqa_hf
+datadir=~/data/linkbert/seqcls/$task
+outdir=runs/$baseline/$task/$MODEL
+mkdir -p $outdir
+python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH \
+  --train_file $datadir/train.json --validation_file $datadir/dev.json --test_file $datadir/test.json \
+  --do_train --do_eval --do_predict \
+  --per_device_train_batch_size 16 --gradient_accumulation_steps 1 --fp16 \
+  --learning_rate 2e-5 --warmup_steps 100 --num_train_epochs 30 --max_seq_length 512 \
+  --save_strategy no --evaluation_strategy no --output_dir $outdir --overwrite_output_dir \
+  --baseline $baseline \
+  |& tee $outdir/log.txt 
+
+############################### QA: BioASQ ###############################
+elif [[ $SGE_TASK_ID -eq 4 ]]; then
+baseline=difflength
+task=bioasq_hf
+datadir=~/data/linkbert/seqcls/$task
+outdir=runs/$baseline/$task/$MODEL
+mkdir -p $outdir
+python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH \
+  --train_file $datadir/train.json --validation_file $datadir/dev.json --test_file $datadir/test.json \
+  --do_train --do_eval --do_predict \
+  --per_device_train_batch_size 16 --gradient_accumulation_steps 1 --fp16 \
+  --learning_rate 2e-5 --warmup_steps 100 --num_train_epochs 20 --max_seq_length 512 \
+  --save_strategy no --evaluation_strategy no --output_dir $outdir --overwrite_output_dir \
+  --baseline $baseline \
+  |& tee $outdir/log.txt 
+
+########### DIFFPERP #############################
+############################### QA: PubMedQA ###############################
+elif [[ $SGE_TASK_ID -eq 5 ]]; then
+baseline=diffperp
+task=pubmedqa_hf
+datadir=~/data/linkbert/seqcls/$task
+outdir=runs/$baseline/$task/$MODEL
+mkdir -p $outdir
+python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH \
+  --train_file $datadir/train.json --validation_file $datadir/dev.json --test_file $datadir/test.json \
+  --do_train --do_eval --do_predict \
+  --per_device_train_batch_size 16 --gradient_accumulation_steps 1 --fp16 \
+  --learning_rate 2e-5 --warmup_steps 100 --num_train_epochs 30 --max_seq_length 512 \
+  --save_strategy no --evaluation_strategy no --output_dir $outdir --overwrite_output_dir \
+  --baseline $baseline \
+  |& tee $outdir/log.txt 
+
+############################### QA: BioASQ ###############################
+elif [[ $SGE_TASK_ID -eq 6 ]]; then
+baseline=diffperp
+task=bioasq_hf
+datadir=~/data/linkbert/seqcls/$task
+outdir=runs/$baseline/$task/$MODEL
+mkdir -p $outdir
+python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH \
+  --train_file $datadir/train.json --validation_file $datadir/dev.json --test_file $datadir/test.json \
+  --do_train --do_eval --do_predict \
+  --per_device_train_batch_size 16 --gradient_accumulation_steps 1 --fp16 \
+  --learning_rate 2e-5 --warmup_steps 100 --num_train_epochs 20 --max_seq_length 512 \
+  --save_strategy no --evaluation_strategy no --output_dir $outdir --overwrite_output_dir \
+  --baseline $baseline \
+  |& tee $outdir/log.txt 
+
+fi
